c17fd27173a15dbad6826b527e0c7b59c9447413
Lisa L. Grotsky <lisa.grotsky@gmail.com>
2018-03-08 21:55:13 -0500

    fixed users links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
 def profile
   @user = current_user
-  @tweets = Tweet.where(user_id: current_user.following.push(current_user))
 end
 
  def all
@@ -10,23 +10,23 @@ end
  end
 
  def show
-   @user = User.find(params['id'])
-   @tweets = @user.tweets
-   render 'profile'
+  @user = User.find(params['id'])
+  @tweets = Tweet.where(user_id: current_user.following.push(current_user))
+  render 'profile'
  end
 
   def follow
       unless current_user.following.include?(params['id'])
-      current_user.following.push(params['id'])
+      current_user.following.push(current_user.id)
       current_user.save!
       end
       redirect_to user_path(params['id'])
   end
 
-def unfollow
-current_user.following.delete(params['id'])
-current_user.save!
-redirect_to user_path(params['id'])
-end
+  def unfollow
+    current_user.following.delete(params['id'])
+    current_user.save!
+    redirect_to user_path(params['id'])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "users/profile" => "users#profile", as: :users_profile
 
   get "/users/all" => "users#all", as: :users
-  get "/users/:id" => 'users#all', as: :user
+  get "/users/:id" => 'users#show', as: :user
 
   get "/follow/:id" => 'users#follow', as: :follow_user
   get "/unfollow/:id" => 'users#unfollow', as: :unfollow_user
